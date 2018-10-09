e6f1dc9d332bf9d5d2d4cd09d3fbe0cbe9ff7d38
artv <vaz985@gmail.com>
2018-10-09 10:53:32 -0300

    arg parser done

--- router.cpp
+++ router.cpp
@@ -12,6 +12,7 @@ using namespace std;
 
 class Router {
   public:
+    string ip;
     uint32_t ip_network, ip_host;
     uint16_t port_network, port_host;
     double period;
@@ -23,6 +24,7 @@ class Router {
     Router(string addr, double period) {
       this->port_host    = (uint16_t)PORT;
       this->port_network = htons(this->port_host);
+      this->ip           = addr;
       this->ip_host      = (uint32_t)stoi(addr);
       this->ip_network   = ntohl(this->ip_host);
       this->period       = period;
@@ -30,9 +32,54 @@ class Router {
       this->setupSocket();
     };
 
+    Router(int argc, char* argv[]) {
+      string addr, filename;
+      string addr_cmd    = "--addr";
+      string upd_cmd     = "--update-period";
+      string startup_cmd = "--startup-commands";
+
+      double period;
+      map<string, string> args;
+      bool startup = false;
+      if(argv[1][0] == '-') {
+        args[string(argv[1])] = string(argv[2]);
+        args[string(argv[3])] = string(argv[4]);
+        if(argc > 5) {
+          args[string(argv[5])] = string(argv[6]);
+          startup = true;
+        }
+      }
+      else {
+        args[addr_cmd] = string(argv[1]);
+        args[upd_cmd]  = string(argv[2]);
+        if(argc > 3) {
+          args[startup_cmd] = string(argv[3]);
+          startup = true;
+        }
+      }
+
+      this->port_host    = (uint16_t)PORT;
+      this->port_network = htons(this->port_host);
+      this->ip         = args[addr_cmd];
+      this->ip_host    = (uint32_t)stoi(this->ip);
+      this->ip_network = ntohl(this->ip_host);
+      this->period     = stod(args[upd_cmd]);
+      this->setupSocket();
+      if(startup) {
+        // chama
+      } 
+    }
+
     ~Router() {
       close(this->udp_socket); 
     };
+
+    void printConfigs() {
+      cout << "IP:     " << this->ip << endl;
+      cout << "Port:   " << this->port_host << endl;
+      cout << "Period: " << this->period << endl;
+    }
+
   private:
     void setupSocket() {
       this->udp_socket = socket(AF_INET, SOCK_DGRAM, 0); 
@@ -47,9 +94,8 @@ class Router {
     };
 };
 
-int main() {
-
-  Router r("127.0.0.1", 1.0);
-
+int main(int argc, char* argv[]) {
+  Router r(argc, argv);
+  r.printConfigs();
   return 0;
 }
