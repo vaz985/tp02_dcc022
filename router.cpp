28fd292e59218344725096f6c37dfe875a385a15
artv <vaz985@gmail.com>
2018-10-13 16:45:46 -0300

    Preparing to class Table, Added argument parser

--- router.cpp
+++ router.cpp
@@ -1,12 +1,13 @@
 #include "tools.h"
+#include "table.h"
 
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <unistd.h>
 
-#define PORT 55151
-#define SOCKETTO 0.5
+const double SOCKETTO 0.5
+const uint16_t PORT 55151
 
 class Router {
   public:
@@ -19,6 +20,8 @@ class Router {
     struct sockaddr_in router_addr;
     unsigned int addr_len = sizeof(struct sockaddr_in);
 
+    class Table table;
+
     Router(string addr, double period) {
       this->port_host    = (uint16_t)PORT;
       this->port_network = htons(this->port_host);
@@ -27,34 +30,19 @@ class Router {
       this->ip_network   = ntohl(this->ip_host);
       this->period       = period;
 
-      this->setupSocket();
+      this->setup_socket();
     };
 
     Router(int argc, char* argv[]) {
-      string addr, filename;
       string addr_cmd    = "--addr";
       string upd_cmd     = "--update-period";
       string startup_cmd = "--startup-commands";
 
-      double period;
-      map<string, string> args;
-      bool startup = false;
-      if(argv[1][0] == '-') {
-        args[string(argv[1])] = string(argv[2]);
-        args[string(argv[3])] = string(argv[4]);
-        if(argc > 5) {
-          args[string(argv[5])] = string(argv[6]);
-          startup = true;
-        }
-      }
-      else {
-        args[addr_cmd] = string(argv[1]);
-        args[upd_cmd]  = string(argv[2]);
-        if(argc > 3) {
-          args[startup_cmd] = string(argv[3]);
-          startup = true;
-        }
-      }
+      vector<string> argv_;
+      for(int i=1; i<argc; i++)
+        argv_.push_back(string(argv[i]));
+
+      map<string, string> args = parse_args(argv_);
 
       this->port_host    = (uint16_t)PORT;
       this->port_network = htons(this->port_host);
@@ -62,23 +50,25 @@ class Router {
       this->ip_host    = (uint32_t)stoi(this->ip);
       this->ip_network = ntohl(this->ip_host);
       this->period     = stod(args[upd_cmd]);
-      this->setupSocket();
-      if(startup) {
-        // chama
-      } 
+      this->setup_socket();
+      this->table.add_edge(this->ip, 0.0);
+      if(args.count(startup_cmd)) {
+        // Run comands
+      }
     }
 
     ~Router() {
       close(this->udp_socket); 
     };
 
-    void printConfigs() {
+    void print_configs() {
       cout << "IP:     " << this->ip << endl;
       cout << "Port:   " << this->port_host << endl;
       cout << "Period: " << this->period << endl;
     }
 
-		void treatInput() {
+
+		void treat_input() {
 			string op_type;
 			cin >> op_type;
 			if(op_type == "add") {
@@ -86,26 +76,57 @@ class Router {
 				cin >> ip;
 				int weight;
 				cin >> weight;
-				addEdge(ip, weight);
+				//addEdge(ip, weight);
 			}
 			else if(op_type == "del"){
 				string ip;
 				cin >> ip;
-				delEdge(ip);
+				//delEdge(ip);
 			}
 			else {
 				cerr << op_type << " is not a valid operation.\n";
 			}
 		}
 
-  private:
-		
-		map<string, int> edges;
+    void send_update_msg() {
+      
+    }
 
-		// For each destination, for each cost, there is a list of possible first edges.
-		map<string, map<int, vector<string> > routes;
-		
-    void setupSocket() {
+    void recv_msg() {
+    }
+
+  private:
+
+    static map<string, string> parse_args(vector<string> &args) {
+      string addr_cmd    = "--addr";
+      string upd_cmd     = "--update-period";
+      string startup_cmd = "--startup-commands";
+      bool error = false;
+      map<string, string> args_;
+      unsigned int args_n = args.size();
+      if(args_n == 2 || args_n == 3) {
+        args_[addr_cmd] = args[0];
+        args_[upd_cmd]  = args[1];
+        if(args_n == 3)
+          args_[startup_cmd] = args[2];
+      }
+      else if(args_n == 4 || args_n == 6) {
+        args_[args[0]] = args[1];
+        args_[args[2]] = args[3];
+        if(args_n == 6)
+          args_[args[4]] = args[5];
+      }
+      else 
+        error = true;
+
+      if(error) {
+        cerr << "Invalid arguments" << endl;
+        exit(1); 
+      }
+      return args_;
+    }
+
+    void setup_socket() {
       this->udp_socket = socket(AF_INET, SOCK_DGRAM, 0); 
 
       this->router_addr.sin_family = AF_INET;
@@ -117,25 +138,13 @@ class Router {
            sizeof(struct sockaddr));
     };
 
-		void addEdge(string ip, int weight) {
-			if(edges.count(ip)) {
-				cerr << "Add to existing ip " << ip << endl;
-				exit(1);
-			}
-			edges[ip] = weight;
-		}
-
-		void delEdge(string ip) {
-			if(!edges.count(ip)) {
-				cerr << "Trying to delete non-existent ip " << ip << endl;
-				exit(1);
-			}
-			edges.erase(ip);
-		}
 };
 
 int main(int argc, char* argv[]) {
   Router r(argc, argv);
-  r.printConfigs();
+  r.print_configs();
+  while(1) {
+    r.recv_msg();
+  }
   return 0;
 }
